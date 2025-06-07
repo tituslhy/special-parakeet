91e0d647438b7a9fae8375a23ad820225e1523ea
Titus Lim <tituslhy@gmail.com>
2025-06-07 22:36:27 +0800

    add llamaindex rag agent on docling

--- src/beeai_agents/agent.py
+++ src/beeai_agents/agent.py
@@ -1,25 +1,98 @@
+## Reference: https://medium.com/mitb-for-all/a-guide-to-code-testing-rag-agents-without-real-llms-or-vector-dbs-51154ad920be
 import os
+import warnings
 from collections.abc import AsyncGenerator
 
-from acp_sdk import MessagePart, Metadata
-from acp_sdk.models import Message
-from acp_sdk.server import Context, RunYield, RunYieldResume, Server
+from acp_sdk import Message, MessagePart, Metadata
+from acp_sdk.server import RunYield, RunYieldResume, Server
+
+from llama_index.core import Settings, VectorStoreIndex
+from llama_index.core.agent.workflow import FunctionAgent, AgentStream
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.node_parser import MarkdownNodeParser
+from llama_index.readers.docling import DoclingReader
+from llama_index.llms.ollama import Ollama
+from llama_index.embeddings.ollama import OllamaEmbedding
+
+warnings.filterwarnings("ignore")
+
+## Load document
+reader = DoclingReader()
+node_parser = MarkdownNodeParser()
+documents = reader.load_data("https://arxiv.org/pdf/2408.09869")
+
+## Create RAG query engine
+# Settings.llm = OpenAI("gpt-4o-mini", temperature=0)
+# Settings.embed_model = OpenAIEmbeddings()
+
+Settings.llm = Ollama("qwen2.5", temperature=0)
+Settings.embed_model = OllamaEmbedding(model_name="nomic-embed-text")
+
+index = VectorStoreIndex.from_documents(
+    documents=documents,
+    transformations=[node_parser]
+)
+query_engine = index.as_query_engine()
+
+## Create the agent
+tools = [
+    QueryEngineTool(
+        query_engine = query_engine,
+        metadata = ToolMetadata(
+            name="Docling_Knowledge_Base",
+            description="Use this tool to answer any questions related to the Docling framework"
+        )
+    )
+]
+agent = FunctionAgent(tools=tools, llm=Settings.llm)
 
 server = Server()
 
-
 @server.agent(
-    metadata=Metadata(ui={"type": "hands-off"})
+    name = "llamaindex_rag_agent",
+    description = "LlamaIndex agent that answers questions about Docling",
+    metadata = Metadata(
+        version="1.0.0",
+        framework="LlamaIndex",
+        programming_language="Python",
+        ui={
+            "type": "chat",
+            "user_greeting": "Hello! What question do you have about the Docling framework?",
+        },
+        author = {
+            "name": "Titus Lim",
+            "email": "tituslhy@gmail.com",
+        },
+        use_cases = [
+            "**Retrieval-Augmented Generation (RAG)**: This agent can be used to answer questions about the Docling framework using the knowledge base.",
+        ],
+        tags = ["RAG"],
+        documentation="""
+        ## LlamaIndex RAG Agent ##
+        
+        This agent uses the LlamaIndex framework to answer questions about the Docling
+        framework using the Docling technical paper as its knowledge base. 
+        
+        The agent is designed to work in streaming mode, providing answers as they
+        are generated.
+        """
+    )
 )
-async def example_agent(input: list[Message], context: Context) -> AsyncGenerator[RunYield, RunYieldResume]:
-    """Polite agent that greets the user"""
-    hello_template: str = os.getenv("HELLO_TEMPLATE", "Ciao %s!")
-    yield MessagePart(content=hello_template % str(input[-1]))
+async def llamaindex_rag_agent(input: list[Message]) -> AsyncGenerator[RunYield, RunYieldResume]:
+    """LlamaIndex agent that answers questions using the  Docling
+    knowledge base. The agent answers questions in streaming mode."""
 
+    query = str(input[-1])
+    handler = agent.run(query)
+    async for ev in handler.stream_events():
+        if isinstance(ev, AgentStream):
+            yield ev.delta
+    response = await handler
+    yield MessagePart(content=str(response))
 
 def run():
     server.run(host=os.getenv("HOST", "127.0.0.1"), port=int(os.getenv("PORT", 8000)))
 
-
 if __name__ == "__main__":
     run()
+    
